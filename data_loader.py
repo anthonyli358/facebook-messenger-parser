0a375d0e5fe84e8f70ca659d47224b3ff528aa60
Unknown <ant385@hotmail.co.uk>
2018-09-15 12:18:41 +0100

    Initial Message Numbers
    - Load all message data using glob
    - Plot message numbers (bar chart plots x alphabetically by default)

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,6 @@
+import glob
 import json
+import os
 
 
 class DataLoader:
@@ -6,10 +8,15 @@ class DataLoader:
     Load data from Facebook Messenger json download.
     """
 
-    def __init__(self, file):
+    def __init__(self, path):
         """
         Data Structure Initialisation
-        :param file: The file to import
+        :param path: The facebook messenger data folder
         """
-        with open(file) as f:
-            self.data = json.load(f)
+        self.message_dict = {}
+        for chat in glob.glob('{}/*/message.json'.format(path)):
+            with open(chat) as f:
+                temp_chat = json.load(f)
+                self.message_dict[temp_chat['title']] = temp_chat
+
+        self.stickers_used = len(os.listdir('{}/stickers_used'.format(path)))
--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-# import...
+from data_loader import DataLoader
+from matplotlib import pyplot as plt
+from collections import OrderedDict
+import heapq
+
 
 ##################################
 # --------Initialisation-------- #
 ##################################
 
+test = DataLoader('messages')
+data = test.message_dict
+
+new = {}
+for key in data.keys():
+    new[key] = len(data[key]['messages'])
+
+largest = heapq.nlargest(50, new, key=new.get)
+
+x = []
+y = []
+for key in largest:
+    x.append(key)
+    y.append(new[key])
 
 #######################
 # --------Run-------- #
 #######################
+
+print(largest)
+
+plt.bar(range(len(x)), list(map(float, y)))
+plt.xticks(range(len(x)), x, rotation=90)
+plt.show()
