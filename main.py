523d6d2c1c893fb1d6689540d5d981180d5b58e6
Anthony <anthonyli358@gmail.com>
2019-11-21 20:28:37 +0000

    Restructure code
    - Pandas dataframes and seaborn

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
-from data_loader import DataLoader
+import glob
+import json
+import pandas as pd
+import seaborn as sns
 from matplotlib import pyplot as plt
-from collections import OrderedDict
-import heapq
+from matplotlib.ticker import FuncFormatter
 
 
-##################################
-# --------Initialisation-------- #
-##################################
+def import_messenger_data(path):
+    message_dict = {}
+    for chat in glob.glob(f'{path}/*/message.json'):
+        with open(chat) as f:
+            temp_chat = json.load(f)
+            message_dict[temp_chat['title']] = temp_chat
+    return message_dict
 
-test = DataLoader('messages')
-data = test.message_dict
 
-new = {}
-for key in data.keys():
-    new[key] = len(data[key]['messages'])
+def plot_ordered_bar(df):
+    df = df.sort_values('message_lengths', ascending=False)
+    # Emojis aren't available, will raise "Glyph missing from current font" errors
+    sns.set(style='whitegrid')
+    f, ax = plt.subplots()
+    sns.barplot(x='message_lengths', y='title', data=df,
+                palette=sns.color_palette("Blues_d", n_colors=50),
+                order=df[:50]['title'][::-1])
+    ax.set(ylim=(0, 50), ylabel='Chat', xlabel='Message Lengths')
+    sns.despine(left=True, bottom=True)
+    ax.xaxis.set_major_formatter(FuncFormatter(lambda x, p: format(int(x), ',')))  # add commas to xlabels
 
-largest = heapq.nlargest(50, new, key=new.get)
 
-x = []
-y = []
-for key in largest:
-    x.append(key)
-    y.append(new[key])
-
-#######################
-# --------Run-------- #
-#######################
-
-print(largest)
-
-plt.bar(range(len(x)), list(map(float, y)))
-plt.xticks(range(len(x)), x, rotation=90)
-plt.show()
+if __name__ == '__main__':
+    # Import data
+    messenger_data = pd.DataFrame.from_dict(import_messenger_data('messages')).T.reset_index(drop=True)
+    # Plot message length data
+    messenger_data['message_lengths'] = messenger_data['messages'].apply(lambda x: len(x))
+    plot_ordered_bar(messenger_data)
