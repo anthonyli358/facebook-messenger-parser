adf48a5e9aab341cc243b65d8b777483b198fb85
Anthony <anthonyli358@gmail.com>
2019-12-03 23:23:31 +0000

    Add group network plot
    - Plot messages for group chats

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
+import datetime
 import glob
 import json
+import networkx as nx
+import numpy as np
 import pandas as pd
 import seaborn as sns
+from collections import Counter
 from matplotlib import pyplot as plt
 from matplotlib.ticker import FuncFormatter
-import datetime
-from collections import Counter
-import networkx as nx
 from config import config  # mask name in separate file
-import os
 
 
 def import_messenger_data(path, split=True):
@@ -31,6 +31,16 @@ def clean_messenger_data(df):
     df['participants'] = df['participants'].apply(lambda x: [{'name': config['name']}] if len(x) < 1 else x)
     return df
 
+
+def features_messenger_data(df):
+    df['message_lengths'] = df['messages'].apply(lambda x: len(x))
+    df['messages/participants'] = df['message_lengths'] / df['participants'].apply(
+        lambda x: len(x))
+    df['time_active_days'] = df['messages'].apply(   # ms to days
+        lambda x: (max([i['timestamp_ms'] for i in x]) - min([i['timestamp_ms'] for i in x])) / (1000 * 60 * 60 * 24))
+    df['time_active_days_log'] = df['time_active_days'].apply(
+        lambda x: 1.001 if x < 1.001 else x)  # Account for cases with invalid log(time_active_days)
+    return df
 
 def plot_ordered_bar(df, x, y, slice=50, annotate=True):
     df = df.sort_values(x, ascending=False)
@@ -82,6 +92,7 @@ def messages_by_month(df, self=False):
             month_messages.append(v // len(row['participants']) if self is False else v)
     month_messages_df = pd.DataFrame({'title': title, 'month': month, 'month_messages/participants': month_messages})
     month_messages_df['month'] = pd.to_datetime(month_messages_df['month'], format='%Y-%m')
+    month_messages_df.fillna(0, inplace=True)
     return month_messages_df
 
 
@@ -100,47 +111,88 @@ def plot_annotated_month_data(df, x, y, label, annotate=True):
     plt.show()
 
 
-def create_network_df(df):
+# def create_network_df(df):
+#     name, connections, messages = [], [], []
+#     for index, row in df.iterrows():
+#         message_counter = Counter([message['sender_name'] for message in row['messages']])
+#         if len(row['participants']) > 1:  # no connections to for self-messages
+#             for i in range(len(row['participants'])):
+#                 name.extend([row['participants'][i]['name']] * (len(row['participants']) - 1))
+#                 connections.extend([i['name'] for i in row['participants'][0:i] + row['participants'][i+1:]])
+#                 messages.extend([message_counter[row['participants'][i]['name']]] * (len(row['participants']) - 1))
+#     network_df = pd.DataFrame({'name': name, 'connections': connections, 'messages': messages})
+#     network_df = network_df.groupby(['name', 'connections']).sum().reset_index()
+#     network_df['log_messages'] = network_df['messages'].apply(lambda x: np.log(x))
+#     return network_df
+
+
+def create_network_group_df(df, slice=50):
+    df['message_lengths'] = df['messages'].apply(lambda x: len(x))
+    df = df.sort_values(['message_lengths'], ascending=False)
+    df = df[:slice]  # plot top 50 groups
     name, connections, messages = [], [], []
     for index, row in df.iterrows():
         message_counter = Counter([message['sender_name'] for message in row['messages']])
-        if len(row['participants']) > 1:  # no connections to for self-messages
+        participants = len(row['participants'])
+        if participants > 2:  # no connections to for self-messages
             for i in range(len(row['participants'])):
-                name.extend([row['participants'][i]['name']] * (len(row['participants']) - 1))
-                connections.extend([i['name'] for i in row['participants'][0:i] + row['participants'][i+1:]])
-                messages.extend([message_counter[row['participants'][i]['name']]] * (len(row['participants']) - 1))
+                name.append(row['participants'][i]['name'])
+                connections.append(row['title'])
+                messages.append(message_counter[row['participants'][i]['name']])
     network_df = pd.DataFrame({'name': name, 'connections': connections, 'messages': messages})
     network_df = network_df.groupby(['name', 'connections']).sum().reset_index()
     return network_df
 
 
+def plot_group_network(df, seed=None, annotate=True):
+    plt.figure()
+    graph = nx.from_pandas_edgelist(df, source='name', target='connections', edge_attr='messages')
+    layout = nx.spring_layout(graph, seed=seed)
+
+    nx.draw_networkx_edges(graph, layout, edge_color='#AAAAAA')
+
+    connections = [node for node in graph.nodes() if node in df['connections'].unique()]
+    weights = [np.log(graph.degree(node, weight='messages') / graph.degree(node)) * 200 for node in graph.nodes() if
+               node in df['connections'].unique()]
+    nx.draw_networkx_nodes(graph, layout, nodelist=connections, node_size=weights, node_color='lightblue')
+
+    people = [node for node in graph.nodes() if node in df['name'].unique() and graph.degree(node) <= 1]
+    nx.draw_networkx_nodes(graph, layout, nodelist=people, node_size=100, node_color='#AAAAAA')  # grey
+
+    high_degree_people = [node for node in graph.nodes() if node in df['name'].unique() and graph.degree(node) > 1]
+    nx.draw_networkx_nodes(graph, layout, nodelist=high_degree_people, node_size=100, node_color='#fc8d62')  # orange
+
+    if annotate:
+        connection_dict = dict(zip(connections, connections))
+        nx.draw_networkx_labels(graph, layout, labels=connection_dict)
+
+    plt.axis('off')
+    plt.title('Facebook Messenger Network')
+
+    plt.show()
+
+
 if __name__ == '__main__':
     # Annotate plots with chat titles?
-    annotate_plots = True
+    annotate_plots = False
 
     # Import data
     messenger_data = pd.DataFrame.from_dict(import_messenger_data('messages')).T.reset_index(drop=True)
     messenger_data = clean_messenger_data(messenger_data)
+    messenger_data = features_messenger_data(messenger_data)
     pd.plotting.register_matplotlib_converters()  # temporary fix to register converters
 
     # Plot message length data
-    messenger_data['message_lengths'] = messenger_data['messages'].apply(lambda x: len(x))
     plot_ordered_bar(messenger_data, 'message_lengths', 'title', slice=50, annotate=annotate_plots)
 
-    # Time active
-    messenger_data['messages/participants'] = messenger_data['message_lengths'] / messenger_data['participants'].apply(
-        lambda x: len(x))
-    messenger_data['time_active_days'] = messenger_data['messages'].apply(   # ms to days
-        lambda x: (max([i['timestamp_ms'] for i in x]) - min([i['timestamp_ms'] for i in x])) / (1000 * 60 * 60 * 24))
-    messenger_data['time_active_days_log'] = messenger_data['time_active_days'].apply(
-        lambda x: 1.001 if x < 1.001 else x)  # Account for cases with invalid log(time_active_days)
+    # Messages vs active time
     plot_annotated_scatter(messenger_data, 'time_active_days', 'messages/participants', 'title',
                            annotate=annotate_plots)
     plot_annotated_scatter(messenger_data, 'time_active_days_log', 'messages/participants', 'title',
                            logx=True, logy=True, annotate=annotate_plots)
 
     # Messages by month
-    month_data = messages_by_month(messenger_data).fillna(0)
+    month_data = messages_by_month(messenger_data)
     plot_annotated_month_data(month_data, 'month', 'month_messages/participants', 'title', annotate=annotate_plots)
 
     # Messages by month rolling average
@@ -149,7 +201,7 @@ if __name__ == '__main__':
     plot_annotated_month_data(month_data, 'month', 'month_messages_roll_av', 'title', annotate=annotate_plots)
 
     # Self messages by month
-    self_month_data = messages_by_month(messenger_data, self=True).fillna(0)
+    self_month_data = messages_by_month(messenger_data, self=True)
     self_month_data = self_month_data.groupby(['month']).sum().reset_index()
     self_month_data['title'] = config['name']
     plot_annotated_month_data(self_month_data, 'month', 'month_messages/participants', 'title', annotate=False)
@@ -160,7 +212,8 @@ if __name__ == '__main__':
     plot_annotated_month_data(self_month_data, 'month', 'month_messages_roll_av', 'title', annotate=False)
 
     # Network plot
-    network_df = create_network_df(messenger_data)
+    network_group_df = create_network_group_df(messenger_data, slice=60)
+    plot_group_network(network_group_df, seed=88, annotate=annotate_plots)
 
     # TODO: Friend network by message counts (node size) group chat common members (edges)
     # TODO: Directed graph or how to deal with messages both ways
