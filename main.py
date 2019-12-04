a836f6bc9ea0dc0a6a09d443c9cc36e97d407fb1
Anthony <anthonyli358@gmail.com>
2019-12-04 00:29:36 +0000

    Add network plot
    - Network plot for basic messages

--- main.py
+++ main.py
@@ -39,8 +39,9 @@ def features_messenger_data(df):
     df['time_active_days'] = df['messages'].apply(   # ms to days
         lambda x: (max([i['timestamp_ms'] for i in x]) - min([i['timestamp_ms'] for i in x])) / (1000 * 60 * 60 * 24))
     df['time_active_days_log'] = df['time_active_days'].apply(
-        lambda x: 1.001 if x < 1.001 else x)  # Account for cases with invalid log(time_active_days)
+        lambda x: 1.001 if x < 1.001 else x)  # account for cases with invalid log(time_active_days)
     return df
+
 
 def plot_ordered_bar(df, x, y, slice=50, annotate=True):
     df = df.sort_values(x, ascending=False)
@@ -111,70 +112,70 @@ def plot_annotated_month_data(df, x, y, label, annotate=True):
     plt.show()
 
 
-# def create_network_df(df):
-#     name, connections, messages = [], [], []
-#     for index, row in df.iterrows():
-#         message_counter = Counter([message['sender_name'] for message in row['messages']])
-#         if len(row['participants']) > 1:  # no connections to for self-messages
-#             for i in range(len(row['participants'])):
-#                 name.extend([row['participants'][i]['name']] * (len(row['participants']) - 1))
-#                 connections.extend([i['name'] for i in row['participants'][0:i] + row['participants'][i+1:]])
-#                 messages.extend([message_counter[row['participants'][i]['name']]] * (len(row['participants']) - 1))
-#     network_df = pd.DataFrame({'name': name, 'connections': connections, 'messages': messages})
-#     network_df = network_df.groupby(['name', 'connections']).sum().reset_index()
-#     network_df['log_messages'] = network_df['messages'].apply(lambda x: np.log(x))
-#     return network_df
-
-
-def create_network_group_df(df, slice=50):
+def create_network_df(df, slice=50, group=False):
     df['message_lengths'] = df['messages'].apply(lambda x: len(x))
     df = df.sort_values(['message_lengths'], ascending=False)
-    df = df[:slice]  # plot top 50 groups
+    df = df[:slice]  # limit nodes to avoid overcrowding
     name, connections, messages = [], [], []
     for index, row in df.iterrows():
         message_counter = Counter([message['sender_name'] for message in row['messages']])
         participants = len(row['participants'])
-        if participants > 2:  # no connections to for self-messages
+        if group:
+            if participants > 2:  # group chats only
+                for i in range(len(row['participants'])):
+                    name.append(row['participants'][i]['name'])
+                    connections.append(row['title'])
+                    messages.append(message_counter[row['participants'][i]['name']])
+        else:
             for i in range(len(row['participants'])):
-                name.append(row['participants'][i]['name'])
-                connections.append(row['title'])
-                messages.append(message_counter[row['participants'][i]['name']])
+                name.extend([row['participants'][i]['name']] * (len(row['participants']) - 1))
+                connections.extend([i['name'] for i in row['participants'][0:i] + row['participants'][i + 1:]])
+                messages.extend([message_counter[row['participants'][i]['name']]] * (len(row['participants']) - 1))
     network_df = pd.DataFrame({'name': name, 'connections': connections, 'messages': messages})
     network_df = network_df.groupby(['name', 'connections']).sum().reset_index()
     return network_df
 
 
-def plot_group_network(df, seed=None, annotate=True):
+def plot_network(df, seed=None, annotate=True, group=False):
     plt.figure()
     graph = nx.from_pandas_edgelist(df, source='name', target='connections', edge_attr='messages')
     layout = nx.spring_layout(graph, seed=seed)
-
     nx.draw_networkx_edges(graph, layout, edge_color='#AAAAAA')
+    if group:
+        connections = [node for node in graph.nodes() if node in df['connections'].unique()]
+        weights = [np.log(graph.degree(node, weight='messages') / graph.degree(node)) * 200 for node in connections]
+        nx.draw_networkx_nodes(graph, layout, nodelist=connections, node_size=weights, node_color='lightblue')
 
-    connections = [node for node in graph.nodes() if node in df['connections'].unique()]
-    weights = [np.log(graph.degree(node, weight='messages') / graph.degree(node)) * 200 for node in graph.nodes() if
-               node in df['connections'].unique()]
-    nx.draw_networkx_nodes(graph, layout, nodelist=connections, node_size=weights, node_color='lightblue')
+        people = [node for node in graph.nodes() if node in df['name'].unique()]
+        nx.draw_networkx_nodes(graph, layout, nodelist=people, node_size=100, node_color='RoyalBlue')
 
-    people = [node for node in graph.nodes() if node in df['name'].unique() and graph.degree(node) <= 1]
-    nx.draw_networkx_nodes(graph, layout, nodelist=people, node_size=100, node_color='#AAAAAA')  # grey
+        high_degree_people = [node for node in graph.nodes() if node in df['name'].unique() and graph.degree(node) > 1]
+        nx.draw_networkx_nodes(graph, layout, nodelist=high_degree_people, node_size=100, node_color='#fc8d62')  # orange
+        if annotate:
+            connection_dict = dict(zip(connections, connections))
+            nx.draw_networkx_labels(graph, layout, labels=connection_dict)
+    else:
+        people = [node for node in graph.nodes() if node in df['name'].unique() and graph.degree(node) <= 1]
+        people_weights = [np.log(max(graph.degree(node, weight='messages'), 1)) * 25 for node in people]
+        nx.draw_networkx_nodes(graph, layout, nodelist=people, node_size=people_weights, node_color='RoyalBlue')
 
-    high_degree_people = [node for node in graph.nodes() if node in df['name'].unique() and graph.degree(node) > 1]
-    nx.draw_networkx_nodes(graph, layout, nodelist=high_degree_people, node_size=100, node_color='#fc8d62')  # orange
-
-    if annotate:
-        connection_dict = dict(zip(connections, connections))
-        nx.draw_networkx_labels(graph, layout, labels=connection_dict)
-
+        high_degree_people = [node for node in graph.nodes() if node in df['name'].unique() and graph.degree(node) > 1]
+        high_degree_people_weights = [np.log(max(graph.degree(node, weight='messages'), 1)) * 25
+                                      for node in high_degree_people]
+        nx.draw_networkx_nodes(graph, layout, nodelist=high_degree_people, node_size=high_degree_people_weights,
+                               node_color='#fc8d62')  # orange
+        if annotate:
+            people_nodes = [node for node in graph.nodes() if node in df['name'].unique()]
+            people_dict = dict(zip(people_nodes, people_nodes))
+            nx.draw_networkx_labels(graph, layout, labels=people_dict)
     plt.axis('off')
     plt.title('Facebook Messenger Network')
-
     plt.show()
 
 
 if __name__ == '__main__':
     # Annotate plots with chat titles?
-    annotate_plots = False
+    annotate_plots = True
 
     # Import data
     messenger_data = pd.DataFrame.from_dict(import_messenger_data('messages')).T.reset_index(drop=True)
@@ -212,10 +213,9 @@ if __name__ == '__main__':
     plot_annotated_month_data(self_month_data, 'month', 'month_messages_roll_av', 'title', annotate=False)
 
     # Network plot
-    network_group_df = create_network_group_df(messenger_data, slice=60)
-    plot_group_network(network_group_df, seed=88, annotate=annotate_plots)
+    network_df = create_network_df(messenger_data, slice=100)
+    plot_network(network_df, seed=88, annotate=annotate_plots)
 
-    # TODO: Friend network by message counts (node size) group chat common members (edges)
-    # TODO: Directed graph or how to deal with messages both ways
-    # TODO: Node size ~log(number of messages)
-    # TODO: Group nodes (friend groups/group chat)
+    # Network group plot
+    network_group_df = create_network_df(messenger_data, slice=60, group=True)
+    plot_network(network_group_df, seed=88, annotate=annotate_plots, group=True)
